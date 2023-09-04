6553d7ed675662fe68eefbd6a6050a836073e764
xiaoheimaoo <737476225@qq.com>
2023-09-04 15:58:02 +0800

    优化

--- src/main/java/umamusume/Test.java
+++ src/main/java/umamusume/Test.java
@@ -2,8 +2,26 @@ package umamusume;
 
 import entity.UserInfo;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static utils.Hikari.getConnection;
+
 public class Test {
-    public void test(UserInfo userInfo){
-        System.out.println("done!");
+    public void test(UserInfo userInfo) throws SQLException {
+        Date date = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String dateStr = format.format(date);
+        Connection conn2 = getConnection();
+        String sql2 = "update `order` set `message`='订单已完成',`status`=2,`complete`=? where `order`=? and `status`=1";
+        PreparedStatement ps2 = conn2.prepareStatement(sql2);
+        ps2.setString(1, dateStr);
+        ps2.setString(2, userInfo.getOrder());
+        ps2.executeUpdate();
+        conn2.close();
+        ps2.close();
     }
 }
--- src/main/java/utils/OrderType.java
+++ src/main/java/utils/OrderType.java
@@ -3,8 +3,14 @@ package utils;
 import entity.UserInfo;
 import umamusume.Test;
 
+import java.sql.SQLException;
+
 public class OrderType {
     public static void type(UserInfo userInfo) {
-        new Test().test(userInfo);
+        try {
+            new Test().test(userInfo);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
